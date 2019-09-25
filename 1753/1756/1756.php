fbeee3a2bddb1a5c894ca98c69ca25e0c0a0e0a4
Mynhien <32814856+Mynhien@users.noreply.github.com>
2019-09-25 08:01:50 +0700

    Commit

---
+++ 1753/1756/1756.php
@@ -0,0 +1,108 @@
+<?php
+
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/'.$pattern_document_root.'(.*)$/';
+
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host.$matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+        require_once $dir_block.'/libs/lessc.inc.php';
+    }
+
+    $less = new lessc();
+    $less->compileFile('less/1756.less', 'css/1756.css');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="<?php echo $url_path; ?>/css/1756.css" rel="stylesheet"/>
+    
+</head>
+<body>
+    <?php include '1756-content.php'; ?>
+</body>
+<div class="container">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="js/jquery-2.1.1.js"></script>
+    <script src="js/jquery.mixitup.min.js"></script>
+    <script src="js/main.js"></script> <!-- Resource jQuery -->
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script>
+      var slider = $("#calculatorSlider");
+        var developerBtn =  $("#developerBtn");
+        var corporateBtn = $("#corporateBtn");
+        var privateBtn = $("#privateBtn");
+        var reseller = $("#resellerEarnings");
+        var client = $("#clientPrice");
+        var percentageBonus = 30; // = 30%
+        var license = {
+        corpo: {
+        active: true,
+        price: 319,
+        },
+        dev: {
+        active: false,
+        price: 149,
+        },
+        priv: {
+        active: false,
+        price: 79,
+        }
+        }
+
+        function calculate(price, value) {
+        client.text((Math.round((price - (value / 100 * price)))) + '$');
+        reseller.text((Math.round(((percentageBonus - value) / 100 * price))) + '$')
+        }
+
+        function reset(price) {
+        slider.val(0);
+        client.text(price + '$');
+        reseller.text((Math.round((percentageBonus / 100 * price))) + '$');
+        }
+        developerBtn.on('click', function(e) {
+        license.dev.active = true;
+        license.corpo.active = false;
+        license.priv.active = false;
+        reset(license.dev.price)
+        });
+        privateBtn.on('click', function(e) {
+        license.dev.active = false;
+        license.corpo.active = false;
+        license.priv.active = true;
+        reset(license.priv.price);
+        });
+        corporateBtn.on('click', function(e) {
+        license.dev.active = false;
+        license.corpo.active = true;
+        license.priv.active = false;
+        reset(license.corpo.price);
+        });
+        slider.on("input change", function(e) {
+        if (license.priv.active) {
+        calculate(license.priv.price, $(this).val());
+        } else if (license.corpo.active) {
+        calculate(license.corpo.price, $(this).val());
+        } else if (license.dev.active) {
+        calculate(license.dev.price, $(this).val());
+        }
+        })
+    </script>
+    </div>
+</html>
